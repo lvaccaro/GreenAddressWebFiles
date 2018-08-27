d4cafbbe464ea52728e949b6b07b43e301822403
Luca Vaccaro <luck87@gmail.com>
2018-08-27 12:03:24 +0200

    Fetch temp updated libwally

--- fetch_libwally.sh
+++ fetch_libwally.sh
@@ -4,6 +4,6 @@ set -e
 
 # Add the wally plugin:
 if [ \! -e libwally-core ]; then
-    git clone https://github.com/ElementsProject/libwally-core.git libwally-core
-    (cd libwally-core && git checkout d0a13e08b51fccf420e2be255e3c959831637aef)
+    git clone https://github.com/lvaccaro/libwally-core.git libwally-core
+    (cd libwally-core && git checkout js_update)
 fi
